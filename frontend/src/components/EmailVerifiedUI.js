f9dd11039ae1b5705412e5f26ed9da7ede434200
GeorgeLu00 <goodmew123@gmail.com>
2020-06-24 18:26:13 -0400

    got email verification working

--- frontend/src/components/EmailVerifiedUI.js
+++ frontend/src/components/EmailVerifiedUI.js
@@ -3,11 +3,38 @@ import React from 'react';
 
 function EmailVerifiedUI()
 {
+    const doEmailVerified = async event =>
+    {
+        console.log("doEmailVerified called");
 
+        var js = '{"_id":"'+ window.location.pathname.slice(19) + '"}';
+
+        console.log(js);
+
+        try
+        {
+            const response = await fetch('http://localhost:5000/api/EmailVerification',{method:'PUT',body:js,headers:{'Content-Type': 'application/json'}});
+
+            console.log("calling EmailVerification api");
+
+            var res = JSON.parse(await response.text());
+
+            console.log(res);
+
+            // window.location.href = '/';
+
+        }
+        catch(e)
+        {
+            console.log("there was an error");
+            console.log(e.toString());
+            return;
+        }
+    }
+    doEmailVerified();
     return(
         <div>
             <h1>YOUR EMAIL HAS BEEN VERIFIED, ROUTING TO HOME PAGE</h1>
-            <h1>{window.location.pathname.slice(19)}</h1>
         </div>
     );
 };

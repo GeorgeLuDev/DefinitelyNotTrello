d9c857f13bbb3e866318bbecd88e744a7bb95494
Angel Mendez <angel.ams.8600@gmail.com>
2020-07-05 21:58:27 -0400

    Worked on Login and Signup screens

--- mobile/App.js
+++ mobile/App.js
@@ -1,18 +1,34 @@
+import 'react-native-gesture-handler';
+
 import * as React from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 
-const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
-});
+import Login from './src/components/Authentication/Login/Login';
+//import AuthenticationScreen from './src/components/Authentication'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { create } from 'react-test-renderer';
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
-    </View>
+
+    <NavigationContainer>
+        <Stack.Navigator>
+        <Stack.Screen name="Auth" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
+    // <Login></Login>
+
+    // <View style={styles.container}>
+    //   <Text style={styles.welcome}>Welcome to React Native!</Text>
+    //   <Text style={styles.instructions}>To get started, edit App.js</Text>
+    //   <Text style={styles.instructions}>{instructions}</Text>
+    // </View>
+    
   );
 }
 

a81bc27b5f72ab956bd3fec94b33c3fe02fdf178
John Cope <johnbentcope@gmail.com>
2020-07-09 21:46:41 -0400

    starting to look definitely not like trello

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,14 +6,14 @@ function Header()
     return(
         <div id="header">  
             <h1>Testing different pages</h1>
-            <a href="/">Root/Sign In</a><br />
-            <a href="/ForgotPassword">ForgotPassword</a><br />
-            <a href="/SignUp">SignUp</a><br />
-            <a href="/ListOfBoards">ListOfBoards</a><br />
-            <a href="/BoardPage">BoardPage</a><br />
-            <a href="/UserSettings">UserSettings</a><br />
-            <a href="/EmailVerification/:id">Email Verification</a><br />
-            <a href="/UpdatePassword/:id">Reset Password</a><br />
+            <a href="/">Root/Sign In</a>
+            <a href="/ForgotPassword">ForgotPassword</a>
+            <a href="/SignUp">SignUp</a>
+            <a href="/ListOfBoards">ListOfBoards</a>
+            <a href="/BoardPage">BoardPage</a>
+            <a href="/UserSettings">UserSettings</a>
+            <a href="/EmailVerification/:id">Email Verification</a>
+            <a href="/UpdatePassword/:id">Reset Password</a>
         </div>
     );
 };
